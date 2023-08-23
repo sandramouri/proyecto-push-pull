36ca5fd2004f7a387d58d38c98f9f13bc876b98c
Sandra <142787724+sandramouri@users.noreply.github.com>
2023-08-23 11:31:53 +0200

    Añadida nota explicativa acerca de *args

--- python/suma_con_args.py
+++ python/suma_con_args.py
@@ -5,6 +5,12 @@ def sumar_con_args(*args):
     return resultado
 
 
+'''
+NOTA Acerca de *args
+
+En *args se recibe un número indeterminado de argumentos desde cualquier
+variable de tipo list o tuple
+'''
 numeros = 1, 2, 3, 4, 5
 resultado = sumar_con_args(*numeros)
 print(f"La suma de los números es: {resultado}")
